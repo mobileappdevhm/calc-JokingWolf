6e193caa1bcc1586002222d0536c88f20e4c6842
Felix Peither <felix@peither.de>
2018-03-25 19:40:44 +0200

    for saving

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+// import 'package:csslib/parser.dart';
+import 'package:math_expressions/math_expressions.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:io/io.dart';
+import 'package:audioplayer/audioplayer.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -26,87 +32,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String gleichung = "";
-  num ergebnis = 0;
+  String gleichung = "0";
+  var ergebnis = 0.0;
 
   var buttonWidth = 50.0, buttonHeight = 50.0;
 
-  void add(){
+  var notFirstInput = false, operatorSet = false;
+
+  void enterNumber(String sign) {
     setState((){
-      gleichung += "+";
-    });
-  }
-  
-  void sub(){
-    setState((){
-      gleichung += "-";
+      if (!notFirstInput) {
+        gleichung = sign;
+        notFirstInput = true;
+      } else {
+        gleichung += sign;
+      }
     });
   }
 
-  void mul(){
+  void enterOperator(String operator) {
     setState((){
-      gleichung += "*";
+      if (operatorSet) {
+        gleichung = gleichung.substring(0, gleichung.length - 1);
+        gleichung += operator;
+      } else {
+        gleichung += operator;
+        operatorSet = true;
+      }
+      // gleichung += operator;
     });
   }
 
-  void div(){
+  void clear(bool all) {
     setState((){
-      gleichung += "/";
-    });
-
-  }
-
-  void nol(){
-    setState((){
-      gleichung += "0";
-    });
-  }
-  void eins(){
-    setState((){
-      gleichung += "1";
-    });
-  }
-  void zwei(){
-    setState((){
-      gleichung += "2";
-    });
-  }
-  void drei(){
-    setState((){
-      gleichung += "3";
-    });
-  }
-  void vier(){
-    setState((){
-      gleichung += "4";
-    });
-  }
-  void fuenf(){
-    setState((){
-      gleichung += "5";
+      if (all)
+        ergebnis = 0.0;
+      gleichung = "0";
+      notFirstInput = false;
+      operatorSet = false;
     });
   }
 
-  void sechs(){
+  void toggle() {
     setState((){
-      gleichung += "6";
+      if (gleichung.startsWith("-")) {
+        gleichung = gleichung.substring(1);
+      } else {
+        gleichung = "-" + gleichung;
+      }
     });
   }
 
-  void sieben(){
+  void calculate() {
     setState((){
-      gleichung += "7";
-    });
-  }
-  void acht(){
-    setState((){
-      gleichung += "8";
-    });
-  }
-
-  void neun(){
-    setState((){
-      gleichung += "9";
+      Parser p = new Parser();
+      Expression exp = p.parse(gleichung);
+      ergebnis = exp.evaluate(EvaluationType.REAL,new ContextModel());
     });
   }
 
@@ -118,34 +99,34 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: new Center(
         child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            new Text("Gleichung: $gleichung"),
-            new Text("$ergebnis"),
+            new Text("Gleichung: $gleichung", style: new TextStyle(fontSize: 20.0)),
+            new Text("Ergebnis: $ergebnis", style: new TextStyle(fontSize: 20.0)),
             new Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 new MaterialButton(
                   child: new Text("1"),
-                  onPressed: eins,
+                  onPressed: () => enterNumber("1"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
                 new MaterialButton(
                   child: new Text("2"),
-                  onPressed: zwei,
+                  onPressed: () => enterNumber("2"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
                 new MaterialButton(
                   child: new Text("3"),
-                  onPressed: drei,
+                  onPressed: () => enterNumber("3"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
                 new MaterialButton(
                   child: new Text("+"),
-                  onPressed: add,
+                  onPressed: () => enterOperator("+"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
@@ -155,55 +136,26 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 new MaterialButton(
-                  child: new Text("1"),
-                  onPressed: eins,
+                  child: new Text("4"),
+                  onPressed: () => enterNumber("4"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
                 new MaterialButton(
-                  child: new Text("2"),
-                  onPressed: zwei,
+                  child: new Text("5"),
+                  onPressed: () => enterNumber("5"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
                 new MaterialButton(
-                  child: new Text("3"),
-                  onPressed: drei,
+                  child: new Text("6"),
+                  onPressed: () => enterNumber("6"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
                 new MaterialButton(
-                  child: new Text("+"),
-                  onPressed: add,
-                  height: buttonHeight,
-                  minWidth: buttonWidth,
-                ),
-              ],
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new MaterialButton(
-                  child: new Text("1"),
-                  onPressed: eins,
-                  height: buttonHeight,
-                  minWidth: buttonWidth,
-                ),
-                new MaterialButton(
-                  child: new Text("2"),
-                  onPressed: zwei,
-                  height: buttonHeight,
-                  minWidth: buttonWidth,
-                ),
-                new MaterialButton(
-                  child: new Text("3"),
-                  onPressed: drei,
-                  height: buttonHeight,
-                  minWidth: buttonWidth,
-                ),
-                new MaterialButton(
-                  child: new Text("+"),
-                  onPressed: add,
+                  child: new Text("-"),
+                  onPressed: () => enterOperator("-"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
@@ -213,26 +165,55 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 new MaterialButton(
-                  child: new Text("1"),
-                  onPressed: eins,
+                  child: new Text("7"),
+                  onPressed: () => enterNumber("7"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
                 new MaterialButton(
-                  child: new Text("2"),
-                  onPressed: zwei,
+                  child: new Text("8"),
+                  onPressed: () => enterNumber("8"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
                 new MaterialButton(
-                  child: new Text("3"),
-                  onPressed: drei,
+                  child: new Text("9"),
+                  onPressed: () => enterNumber("9"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
                 new MaterialButton(
-                  child: new Text("+"),
-                  onPressed: add,
+                  child: new Text("*"),
+                  onPressed:() => enterOperator("*"),
+                  height: buttonHeight,
+                  minWidth: buttonWidth,
+                ),
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text("+/-"),
+                  onPressed: toggle,
+                  height: buttonHeight,
+                  minWidth: buttonWidth,
+                ),
+                new MaterialButton(
+                  child: new Text("0"),
+                  onPressed: () => enterNumber("0"),
+                  height: buttonHeight,
+                  minWidth: buttonWidth,
+                ),
+                new MaterialButton(
+                  child: new Text("."),
+                  onPressed: () => enterOperator("."),
+                  height: buttonHeight,
+                  minWidth: buttonWidth,
+                ),
+                new MaterialButton(
+                  child: new Text("/"),
+                  onPressed: () => enterOperator("/"),
                   height: buttonHeight,
                   minWidth: buttonWidth,
                 ),
@@ -243,7 +224,28 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 new MaterialButton(
                   child: new Text("C",style: new TextStyle(color: Colors.red)),
-                  onPressed: () => {},
+                  height: buttonHeight,
+                  minWidth: buttonWidth,
+                  onPressed: () => clear(false),
+                ),
+                new MaterialButton(
+                  child: new Text("CE",style: new TextStyle(color: Colors.red)),
+                  height: buttonHeight,
+                  minWidth: buttonWidth,
+                  onPressed: () => clear(true),
+                ),
+                new MaterialButton(
+                  child: new Text("E",style: new TextStyle(color: Colors.red)),
+                  height: buttonHeight,
+                  minWidth: buttonWidth,
+                  onPressed: () => enterNumber(ergebnis.toString())
+                ),
+                new MaterialButton(
+                  color: Colors.blue,
+                  child: new Text("=", style: new TextStyle(color: Colors.white)),
+                  height: buttonHeight,
+                  minWidth: buttonWidth,
+                  onPressed: calculate,
                 ),
               ],
             ),
