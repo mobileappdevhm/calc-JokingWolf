28227a49520ba591dca3b0965412d6b4252e9fe9
Felix Peither <felix@peither.de>
2018-03-25 20:00:08 +0200

    for saving

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'MyHomePage.dart';
 
-// import 'package:csslib/parser.dart';
-import 'package:math_expressions/math_expressions.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:io/io.dart';
-import 'package:audioplayer/audioplayer.dart';
-
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
